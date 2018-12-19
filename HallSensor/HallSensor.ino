a0e540b34a6d356b15c11a79d5d17c719f452c45
Sanuj Kulshrestha <sanujkul@github.com>
2018-12-19 20:28:03 +0530

    Changed the code logic after discussion

--- HallSensor/HallSensor.ino
+++ HallSensor/HallSensor.ino
@@ -1,65 +1,45 @@
-
-
-/*
- * Simple code to detect magnet and increase counter 
- * After every "resetInitialCountTime" seconds initialCount will will be pivot to current count
- * WHAT I DID:
- * I keep 
- */
-#include <TimerOne.h>                             //Adding the Software Interrupt Library
-#include <PinChangeInt.h>                         //Adding the Hardware interrupt Library
-
-int resetInitialCountTime = 3000000;              //3000000 microsec or 3 sec
-int numberOfMagnets = 3;                          //Number of magnets on fan 
-
+#define NUMBEROFMAGNETS 3           //Number of magnets on fan 
 int dPin = 2;   
-
-int count = 0;
-
-uint32_t initialCount = 0;            //Pivoting Value after every 3 second to compare count and calculating RPM
-uint16_t initialCountTime = 0;        //This will store the time when we have reset our initial pivot
+volatile unsigned long count = 0;
 
 //=====================>> SETUP <<======================================
 void setup() {
+//  Serial.begin(9600);
   pinMode(dPin, INPUT);
-  Timer1.initialize(resetInitialCountTime);   //Initializing Software Interrupts
-  Timer1.attachInterrupt(resetInitialCount);    //Attaching software interrupt
-  
-  attachInterrupt(digitalPinToInterrupt(dPin), raiseCount, FALLING); //Attaching the interrupts
-  count = 0;                                                         //Initializing count
+  count = 0;                        //Initializing count
 }
 //=====================>> SETUP ENDS <<======================================
 //=====================>> LOOP <<======================================
 void loop() {
-  float rpm = giveRPM();
+  attachInterrupt(digitalPinToInterrupt(dPin), raiseCount, FALLING); //Attaching the interrupts
+  volatile unsigned long interruptStartTime = millis()/1000;                   //Time (in second) when we started our interrupts
+  /*
+   * Some other Sensor codes here
+   */
+   
+  long rpm = giveRPM(interruptStartTime);
+  detachInterrupt(digitalPinToInterrupt(dPin));             //Detaching the interrupt
+  count = 0;                                                //Reseting count to 0
+  
 }
 //=====================>> LOOP ENDS <<=================================
+
 /*
- * This function is called every time hall sensor detects magnets.
  * Called by Hardware Interrupt at digital pin
  * Function: Increase global variable count by 1
  */
 int raiseCount(){                             
-  count += 1;
+  count++;
 }
 
-/*
- * Called by software interrupts: 
- * Function: to reset the initialCount and Time
- */
-void resetInitialCount(){            
-  initialCount = count;
-  initialCountTime = millis();
-}
 
 /*
  * RPM = number of rotations/time taken * 60
- *     = (count2 - count1)/(time2 - time1) * 60 .   (count is count per magnet)
+ *     = (count2)/(currentTime - time1) * 60 .   (count is count per magnet)
  */
-float giveRPM(){
-  int numCounts = count - initialCount;                           //Comparing with pivot point- Finding difference between two counts
-  float timeTaken = (float)(millis() - initialCountTime)/100.0;   //Converting unit of timeTaken in second after finding Time difference
-  float noOfRotations = (float)numCounts/numberOfMagnets;         //Detection of "numberOfMagnets" of counts equals 1 rotation
+long giveRPM(volatile unsigned long interruptStartTime){
+  volatile unsigned long timeTaken = ((millis()/1000) - interruptStartTime); 
+  volatile unsigned long noOfRotations = count/NUMBEROFMAGNETS;    //Detection of "numberOfMagnets" of counts equals 1 rotation in that time
   float rpm = (noOfRotations/timeTaken)*60;                       //Maths to get RPM
   return rpm;
 }
