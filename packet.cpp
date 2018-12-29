a0fceea95c17d907946ba9160dc8d85db77023ee
Shikhar1998 <shikharmakhija2@gmail.com>
2018-12-29 18:05:36 +0530

    Create packet.cpp

---
+++ packet.cpp
@@ -0,0 +1,44 @@
+#include <Arduino.h>
+#include "packet.h"
+
+//constructor
+packet::packet(const int team_id){
+	this->team_id = team_id;
+	this->mission_time = 0;
+	this->packet_count = 0;
+	this->altitude = 0;
+	this->pressure;
+	this->temperature;
+	this->voltage;
+	this->gps_time;
+	this->gps_lattitude;
+	this->gps_longitude;
+	this->gps_sats;
+	this->pitch;
+	this->roll;
+	this->blade_spin_rate;
+	this->software_state = boot;
+	this->bonus_direction;
+}
+
+String packet::toString(){
+	String csv = String( 
+	  String(this->team_id) + "," +
+		String(this->mission_time) + "," +
+		String(this->packet_count) + "," +
+		String(this->altitude) + "," +
+		String(this->pressure) + "," +
+		String(this->temperature) + "," +
+		String(this->voltage) + "," +
+		this->gps_time + "," +
+		String(this->gps_lattitude) + "," +
+		String(this->gps_longitude) + "," +
+		String(this->gps_sats) + "," +
+		String(this->pitch) + "," +
+		String(this->roll) + "," +
+		String(this->blade_spin_rate) + "," +
+		op_state_string_lut[this->software_state] + "," +
+		String(this->bonus_direction)
+		);
+    return csv;
+}
